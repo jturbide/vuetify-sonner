0a24bd7e29e021b840cab676e3c21c189f95b92b
Julien Turbide <moi@jturbide.com>
2024-05-03 21:04:39 -0400

    Fix typings: Make optional properties in Options type
    This addresses an issue with the typings in the Options type. Currently, certain properties in the Options type are incorrectly marked as required, causing TypeScript errors when using the plugin in certain scenarios.

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,7 @@ import VSonner from './VSonner.vue'
 import Toast from './Toast.vue'
 import type { ToastProps } from './types'
 
-type Options = Omit<ToastProps, 'text'> & Pick<ExternalToast, 'duration' | 'onAutoClose' | 'onDismiss' | 'id' | 'important'>
+type Options = Partial<Omit<ToastProps, 'text'>> & Pick<ExternalToast, 'duration' | 'onAutoClose' | 'onDismiss' | 'id' | 'important'>;
 
 function toastFunction(text: string, options?: Options) {
   const { description, action, ...rest } = options || {}
